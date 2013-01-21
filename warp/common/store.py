04ab4f02b1fc606b4b1dfb6dcdb1badcfb276a78
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-01-21 22:10:17 +0700

    don't try creating tables for postgres/mysql inside warp.common.store.setupStore (schemup handles these now)

--- warp/common/store.py
+++ warp/common/store.py
@@ -14,7 +14,11 @@ def setupStore():
         from storm.tracer import debug
         debug(True, stream=sys.stdout)
 
+    # Only sqlite uses this now
     sqlBundle = getCreationSQL(avatar_store)
+    if not sqlBundle:
+        return
+
     tableExists = sql['tableExists'] = sqlBundle['tableExists']
 
     for (table, creationSQL) in sqlBundle['creations']:
@@ -32,36 +36,6 @@ def setupStore():
 def getCreationSQL(store):
     connType = store._connection.__class__.__name__
     return {
-        'PostgresConnection': {
-            'tableExists': lambda s, t: bool(s.execute("SELECT count(*) FROM pg_class where relname = ?::text", (unicode(t),)).get_one()[0]),
-            'creations': [
-                ('warp_avatar', """
-                CREATE TABLE warp_avatar (
-                    id SERIAL NOT NULL PRIMARY KEY,
-                    email VARCHAR,
-                    password VARCHAR,
-                    UNIQUE(email))"""),
-                ('warp_session', """
-                CREATE TABLE warp_session (
-                    uid BYTEA NOT NULL PRIMARY KEY,
-                    isPersistent BOOLEAN NOT NULL DEFAULT FALSE,
-                    touched INTEGER,
-                    avatar_id INTEGER REFERENCES warp_avatar(id) ON DELETE CASCADE)"""),
-                ('warp_avatar_role', """
-                CREATE TABLE warp_avatar_role (
-                    id SERIAL NOT NULL PRIMARY KEY,
-                    avatar_id INTEGER NOT NULL REFERENCES warp_avatar(id) ON DELETE CASCADE,
-                    role_name BYTEA NOT NULL,
-                    position SERIAL NOT NULL)"""),
-                ('warp_fulltext', (
-                """CREATE TABLE warp_fulltext (
-                    model VARCHAR(128) NOT NULL,
-                    doc_id INTEGER NOT NULL,
-                    fulltext tsvector,
-                    PRIMARY KEY (model, doc_id))""",
-                 """CREATE INDEX fulltext_idx ON warp_fulltext USING gin(fulltext)""")),
-                ],
-            },
         'SQLiteConnection': {
             'tableExists': lambda s, t: bool(s.execute("SELECT count(*) FROM sqlite_master where name = '%s'" % t).get_one()[0]),
             'creations': [
@@ -82,34 +56,5 @@ def getCreationSQL(store):
                     role_name BYTEA NOT NULL,
                     position INTEGER NOT NULL DEFAULT 0)"""),
                 ],
-            },
-        'MySQLConnection': {
-            'tableExists': lambda s, t: bool(s.execute("""
-                   SELECT count(*) FROM information_schema.tables
-                   WHERE table_schema = ? AND table_name=?""",
-               (URI(config['db']).database, t)).get_one()[0]),
-            'creations': [
-                ('warp_avatar', """
-                CREATE TABLE warp_avatar (
-                    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    email VARCHAR(64),
-                    password VARCHAR(32),
-                    UNIQUE(email)
-                  ) engine=InnoDB, charset=utf8"""),
-                ('warp_session', """
-                CREATE TABLE warp_session (
-                    uid VARBINARY(32) NOT NULL PRIMARY KEY,
-                    avatar_id INTEGER REFERENCES warp_avatar(id) ON DELETE CASCADE
-                  ) engine=InnoDB, charset=utf8"""),
-                ('warp_avatar_role', """
-                CREATE TABLE warp_avatar_role (
-                    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    avatar_id INTEGER NOT NULL REFERENCES warp_avatar(id) ON DELETE CASCADE,
-                    role_name VARBINARY(32) NOT NULL,
-                    position INTEGER NOT NULL
-                  ) engine=InnoDB, charset=utf8"""),
-                ],
-            },
-    }[connType]
-
-
+            }
+    }.get(connType)
