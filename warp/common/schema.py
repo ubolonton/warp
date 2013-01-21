b579d240557670682d76ab167a37e77e7e14ac95
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-01-21 18:51:27 +0700

    add migrate & snapshot functions

--- warp/common/schema.py
+++ warp/common/schema.py
@@ -1,7 +1,9 @@
 from warp.runtime import avatar_store, config
 
 from schemup.orms import storm
-from schemup import commands
+from schemup import commands, validator
+
+# NTA TODO: Maybe load once?
 
 stormSchema = storm.StormSchema()
 
@@ -13,13 +15,31 @@ def getWarpMigrationsDir(store=avatar_store):
         "MySQLConnection": "mysql"
     }[store._connection.__class__.__name__])
 
+def getClass(store=avatar_store):
+    name = store._connection.__class__.__name__
+    if name == "PostgresConnection":
+        from schemup.dbs.postgres import PostgresSchema
+        return PostgresSchema
+    elif name == "MySQLConnection":
+        from schemup.dbs.mysql import MysqlSchema
+        return MysqlSchema
+    else:
+        raise Exception("Unsupported DB")
+
+def makeSchema(store=avatar_store, dryRun=False):
+    schemaClass = getClass()
+    # return schemaClass(store._connection._raw_connection, dryRun=dryRun)
+    return schemaClass(store.get_database().raw_connect(), dryRun=dryRun)
+
 # NTA TODO: All DB-related config should be grouped together
-def getSchemaConfig(config=config):
+def getConfig(config=config):
     schemaConfig = {
         "migrations_dir": config["siteDir"].child("migrations")
     }
     schemaConfig.update(config.get("schema", {}))
     return schemaConfig
+
+
 
 def loadWarpMigrations(store=avatar_store):
     warpMigrationsDir = getWarpMigrationsDir(store)
@@ -28,12 +48,49 @@ def loadWarpMigrations(store=avatar_store):
     commands.load(warpMigrationsDir.path)
 
 def loadSiteMigrations(config=config):
-    schemaConfig = getSchemaConfig(config)
+    schemaConfig = getConfig(config)
     siteMigrationsDir = schemaConfig["migrations_dir"]
     # NTA TODO: Blow up otherwise?
     if siteMigrationsDir.isdir():
         commands.load(siteMigrationsDir.path)
 
-def loadMigrations():
+def loadMigrations(store=avatar_store, config=config):
     loadWarpMigrations(avatar_store)
     loadSiteMigrations(config)
+
+
+
+def snapshot(store=avatar_store, config=config, dryRun=False):
+    schema = makeSchema(store, dryRun)
+    loadMigrations(store, config)
+
+    schema.ensureSchemaTable()
+    commands.snapshot(schema, stormSchema)
+
+def migrate(store=avatar_store, config=config, dryRun=False):
+    schema = makeSchema(store, dryRun)
+    schema.ensureSchemaTable()
+
+    # Make sure the real schema is what schemup_tables says it is
+    mismatches = validator.findSchemaMismatches(schema)
+    # NTA TODO: Pretty print
+    if mismatches:
+        print "Real schema & 'schemup_tables' are out of sync (did you change the schema outside of schemup?):"
+        for mismatch in mismatches:
+            print mismatch, "\n"
+        raise Exception("Schema mismatches")
+
+    loadMigrations(store, config)
+
+    sqls = commands.upgrade(schema, stormSchema)
+    if dryRun and sqls:
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        for sql in sqls: print sql
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+    # Sanity checking
+    commands.validate(schema, stormSchema)
+
+    if not dryRun:
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "Migrated successfully"
